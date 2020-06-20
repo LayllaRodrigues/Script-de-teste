23e06f0b51c2cbefb163699a2496f3044a6d974b
Laylla <laylla.rodrigues@youse.com.br>
2020-06-20 13:10:35 -0300

    preenchimento de dropdown - sample e find

--- spec/dropdown_spec.rb
+++ spec/dropdown_spec.rb
@@ -7,4 +7,15 @@ describe 'Caixa de seleção', :drop do
         select('Loki',from:'dropdown')
         sleep 3
     end
+
+    it 'item especifico com o find' do
+        visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+        dropd = find('.avenger-list')
+        dropd.find('option',text: 'Scott Lang').select_option
+    end
+    it 'qualquer item', :sample do 
+        visit 'https://training-wheels-protocol.herokuapp.com/dropdown'
+        dropd = find('.avenger-list')
+        dropd.all('option').sample.select_option
+    end
 end
