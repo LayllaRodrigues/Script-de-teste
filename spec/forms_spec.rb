e22273cd55e45c1957c74becbf41f06722d1a337
Laylla <laylla.rodrigues@youse.com.br>
2020-06-20 12:07:20 -0300

    opção 2 de verificação

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -8,6 +8,7 @@ describe 'forms' do
         click_button 'Login'
 
         expect(find('#flash').visible?).to be true #o expect é a melhor opção dado que tem outros caracteres junto com a msg que vou consultar (area logada)
-        expect(find("#flash").text).to include "Olá, Tony Stark. Você acessou a área logada!" #opção 1
+        expect(find('#flash').text).to include "Olá, Tony Stark. Você acessou a área logada!" #opção 1
+        expect(find('#flash')).to have_content "Olá, Tony Stark. Você acessou a área logada!" #opção 2 verifica se existe ese txt no site
     end
 end
